b1e02ffca9b5faf32377b54cfde6a8adb3896841
Mira Kim <mira.kim@thoughtworks.com>
2020-05-07 23:34:44 +1000

    Integrate with DialogFlow using fetch api

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 import React, {useState} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
+import { DF_ACCESS_TOKEN } from 'react-native-dotenv'
+
+const createChatbotMessage = (id, messageText) => {
+  return {
+    _id: id,
+    text: messageText,
+    createdAt: new Date(),
+    user: {
+      _id: 2,
+      name: 'Chatbot',
+      avatar: 'https://placeimg.com/140/140/any',
+    },
+  }
+};
 
 const App = () => {
   const [messages, setMessages] = useState([
@@ -15,8 +29,18 @@ const App = () => {
     },
   ]);
 
+
+
   const onSend = (newMessages = []) => {
-    setMessages(GiftedChat.append(messages, newMessages));
+    const temp = GiftedChat.append(messages, newMessages);
+    setMessages(temp);
+    const messageId = newMessages[0]._id + 1;
+    sendMessageToChatbot(newMessages[0].text)
+    .then((json) => {
+      console.log(json);
+
+      setMessages(GiftedChat.append(temp, createChatbotMessage(messageId, json.queryResult.fulfillmentText)));
+    })
   };
 
   return (
@@ -29,5 +53,32 @@ const App = () => {
     />
   );
 };
+
+function sendMessageToChatbot(message) {
+  const projectId = "try-chatbot-272523";
+  const sessionId = "ksjdhuh23uhkwjdhf";
+  const authHeader = `Bearer ${DF_ACCESS_TOKEN}`;
+  console.log(authHeader);
+  return fetch(`https://dialogflow.googleapis.com/v2/projects/${projectId}/agent/sessions/${sessionId}:detectIntent`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: authHeader
+    },
+    body: JSON.stringify({
+      "query_input": {
+        "text": {
+          "text": message,
+          "language_code": "en-US"
+        }
+      }
+    }),
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 export default App;
